6f6b474d82efc23027b94344da3f32ea07e26017
Giovanni Olan Perez <giovanniolanperez.fp@gmail.com>
2025-01-31 17:13:11 +0000

    Clean inital project and instaling angular vscode extension

--- deepl-clone/src/app/app.component.ts
+++ deepl-clone/src/app/app.component.ts
@@ -4,8 +4,9 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <h1>hola mundo</h1>
+  `,
 })
 export class AppComponent {
   title = 'deepl-clone';
